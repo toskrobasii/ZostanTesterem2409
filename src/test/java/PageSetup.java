53dc668694ce72f3886b3a2f242dd0936f8e3ffe
Tomasz Oskroba <toskroba@sii.pl>
2023-09-24 16:44:17 +0200

    New tests implemented

---
+++ src/test/java/PageSetup.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PageSetup {
+
+    // typy zmiennych proste i obiektowe
+    // przeciazanie metod - tzw. overload
+    String projectLocation = System.getProperty("user.dir");
+    ChromeDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", projectLocation + "/resources/chromedriver117.exe");
+        driver = new ChromeDriver();
+
+        // Page Object Model
+        driver.manage().window().maximize();
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+    }
+
+//    @AfterEach
+//    public void teardown() {
+//        // zmienne lokalne i globalne
+//        driver.quit();
+//    }
+}
---
+++ src/test/java/UltimateQATest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// modyfikatory dostepu (public / private / protected)
+public class UltimateQATest extends PageSetup {
+
+    @Test
+    public void testOne() throws InterruptedException {
+        WebElement button = driver.findElementById("idExample");
+        button.click();
+
+        // Waity w Selenium: implicit wait, explicit wait, fluent wait
+        Thread.sleep(2000);
+
+        WebElement text = driver.findElementByClassName("entry-title");
+        Assertions.assertEquals("Button success", text.getText());
+    }
+
+
+    @Test
+    public void testTwo() throws InterruptedException {
+        WebElement nameInput = driver.findElementById("et_pb_contact_name_0");
+        nameInput.sendKeys("Tester");
+
+        Thread.sleep(2000);
+
+        WebElement emailInput = driver.findElementById("et_pb_contact_email_0");
+        emailInput.sendKeys("tester@tester.pl");
+
+        Thread.sleep(2000);
+
+        WebElement buttonEmailMe = driver.findElementByName("et_builder_submit_button");
+        buttonEmailMe.click();
+    }
+
+
+}
